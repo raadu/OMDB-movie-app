b331a1a9360c67f714bb5d00fbab0bb977db2646
Raiyad Raad <raiyad.raad@gmail.com>
2021-02-27 11:45:44 +0600

    routing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login/Login';
+import Dashboard from './Dashboard/Dashboard';
+import ErrorPage from './ErrorPage/ErrorPage';
+import {
+  BrowserRouter, 
+  Route, 
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Login}
+        />
+        <Route
+          exact
+          path="/dashboard"
+          component={Dashboard}
+        />
+        <Route
+          path="/"
+          component={ErrorPage}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
